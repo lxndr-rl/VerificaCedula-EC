9d28d6b66c2d955730e64573c7d2446c3ec2c983
Alexander <47733425+lxndr-rl@users.noreply.github.com>
2022-02-27 21:36:07 -0500

    Cédula con digVerif = 0
    Issue: #1
    Cambiado TryParse por RegEx porque ciertos casos marcaba como false algunos números enteros

--- Program.cs
+++ Program.cs
@@ -1,43 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
-namespace VerificaCedula
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string cedula;
+namespace VerificaCedula {
+    class Program {
+        static void Main(string[] args) {
+            string cedula = "a";
 
-            Console.WriteLine("Ingrese su número de cédula");
-            cedula = Console.ReadLine();
-
-
-            if (int.TryParse(cedula, out int output) && ValidaLongitud(cedula) && ValidarProvincia(cedula) && ValidaTercer(cedula) && ValidaUltimoDigito(cedula))
-            {
-                Console.WriteLine("La cédula es válida");
-            }
-            else
-            {
-                Console.WriteLine("La cédula no es Válida");
+            while (cedula != "0") {
+                Console.WriteLine("Ingrese su número de cédula");
+                cedula = Console.ReadLine();
+                if (Regex.IsMatch(cedula, @"^\d+$") && ValidaLongitud(cedula) && ValidarProvincia(cedula) && ValidaTercer(cedula) && ValidaUltimoDigito(cedula)) {
+                    Console.WriteLine("\nLa cédula es válida");
+                } else {
+                    Console.WriteLine("\nLa cédula no es Válida");
+                }
             }
         }
 
-        private static bool ValidaLongitud(string cedula)
-        {
-            if (cedula.Length < 10 || cedula.Length > 10)
-            {
+        private static bool ValidaLongitud(string cedula) {
+            if (cedula.Length < 10 || cedula.Length > 10) {
                 Console.WriteLine("LONGITUD INVALIDO");
                 return false;
-            }
-            else
-            {
+            } else {
                 Console.WriteLine("LONGITUD VALIDO");
                 return true;
             }
         }
-        private static bool ValidarProvincia(string cedula)
-        {
+        private static bool ValidarProvincia(string cedula) {
             int dosDigitos = Convert.ToInt32(cedula.Substring(0, 2));
             if ((dosDigitos < 1 || dosDigitos > 24) && dosDigitos != 30)        //Los dos primeros dígitos indican la provincia (01-24 y 30 para extranjeros) 
             {
@@ -48,8 +38,7 @@ namespace VerificaCedula
             return true;
         }
 
-        private static bool ValidaTercer(string cedula)
-        {
+        private static bool ValidaTercer(string cedula) {
             char digito = cedula[2];
             if (digito >= '0' && digito <= '6')            //El tercer dígito debe estar entre 0 y 6 incluídos
             {
@@ -60,49 +49,45 @@ namespace VerificaCedula
             return false;
         }
 
-        private static bool ValidaUltimoDigito(string cedula)
-        {
+        private static bool ValidaUltimoDigito(string cedula) {
             List<char> PosPar = new List<char>();
             List<char> PosImpar = new List<char>();
 
             int respImp = 0;
             int respPar = 0;
             string nueveDig = cedula.Substring(0, 9);
-            for (int i = 0; i < nueveDig.Length; i += 2)
-            {
+            for (int i = 0; i < nueveDig.Length; i += 2) {
                 PosImpar.Add(nueveDig[i]);
             }
-            for (int i = 1; i < nueveDig.Length; i += 2)
-            {
+            for (int i = 1; i < nueveDig.Length; i += 2) {
                 PosPar.Add(nueveDig[i]);
             }
 
-            foreach (char num in PosImpar)
-            {
+            foreach (char num in PosImpar) {
                 int resp = int.Parse(num.ToString()) * 2;
-                if (resp > 9)
-                {
+                if (resp > 9) {
                     resp -= 9;
                 }
                 respImp += resp;
             }
 
-            foreach (char num in PosPar)
-            {
+            foreach (char num in PosPar) {
                 respPar += int.Parse(num.ToString());
             }
 
-            int moduloDiez = 10 - (respImp + respPar) % 10;
+            int moduloDiez;
+            if ((respImp + respPar) % 10 != 0) {
+                moduloDiez = 10 - (respImp + respPar) % 10;
+            } else {
+                moduloDiez = (respImp + respPar) % 10;
+            }
 
             Console.WriteLine($"El dígito verificador de {nueveDig} es {moduloDiez}");
 
-            if (int.Parse(cedula[9].ToString()) == moduloDiez)
-            {
+            if (int.Parse(cedula[9].ToString()) == moduloDiez) {
                 Console.WriteLine("MODULO 10 VALIDO");
                 return true;
-            }
-            else
-            {
+            } else {
                 Console.WriteLine("MODULO 10 INVALIDO");
                 return false;
             }
